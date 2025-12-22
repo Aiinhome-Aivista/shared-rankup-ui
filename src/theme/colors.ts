30e09291ff304c8542f4f2b3e55f57b349f7b29c
Pinki Paul <pp3785828@gmail.com>
2025-12-22 12:39:48 +0530

    color define

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,23 +2,32 @@ export const palette = {
   common: {
     white: "#FFFFFF",
     black: "#000000",
+    blue:  "#514CF1",
   },
   purple: {
-    50: "#EBEBFF",
+    50:  "#EBEBFF",
     100: "#D6D6FF",
-    200: "#C2C2FF", 
-    300: "#B4B4E6",
-    500: "#5B58F3",
+    150: "#C2C2FF", 
+    200: "#B4B4E6",
+    300: "#A2AEF2",
+    350: "#A1AEF2",
+    400: "#A1AEF2B2",
+    450: "#7E8FF2",
+    500: "#797CF1",
+    550: "#5B58F3",
     600: "#4845C9",
+    650: "#514CF180",
     700: "#3A37A5",
+    750: "#2F2C8B",
+    800: "#514CF133",
+    850: "#514CF11A", 
+    900: "#514CF103",
   },
   green: {
     900: "#142210", // Bg Default
     850: "#1C2E18", // Surface
     800: "#253820", // Subtle
     700: "#2C4226",
-    
-    // Neon Actions (Dark Theme Primary)
     400: "#4ADE80", 
     500: "#22C55E",
     600: "#16A34A",
@@ -30,17 +39,36 @@ export const palette = {
     successBgDark: "#143318",
   },
   red: {
-    500: "#EF4444", // Error Default
-    50: "#FEF2F2",   // Error Bg Light
+    500: "#EF4444",
+    600: "#F21D2F", // Error Default
+    50:  "#FEF2F2",   // Error Bg Light
     900: "#451A1A",  // Error Bg Dark
   },
   neutral: {
-    50: "#F5F6FA", // Bg Light Default
-    100: "#F0F2F5", // Subtle Light
-    200: "#E5E7EB", // Border Light
-    300: "#E1E4EA", // Muted Light
-    500: "#9CA3AF", // Text Muted Dark
-    600: "#6B7280", // Text Muted Light
+    50:  "#F5F6FA", // Bg Light Default
+    100: "#F0F2F5",
+    200: "#E5E7EB",
+    300: "#E1E4EA",
+    350: "#D9D9D9",
+    400: "#9CA3AF",
+    450: "#888585",
+    500: "#FFFFFF80",
+    550: "#6B7280",
+    600: "#FFFFFF38",
+    650: "#FFFFFF40",
+    700: "#A1AEF226",
+    750: "#FFFFFF1A",
+    800: "#FFFFFF0D",
+    850: "#514CF105", // Text Muted Light
     900: "#1B1B3A", // Text Default Light
-  }
+  },
+  black:{
+    500:"#262626",
+    600:"#1C1B1F",
+    700:"#00000040",
+  },
+  yellow: {
+    300: "#F2EB1D",
+   
+  },
 };

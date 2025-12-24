a5ba2f605cc6b07227d455dec918b36b8741d957
Pinki Paul <pp3785828@gmail.com>
2025-12-24 12:35:12 +0530

    icon package

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from "./components/Card";
 export * from "./components/Calendar";
 export * from "./components/icons/ArrowForward";
 export * from "./components/icons/ArrowBack";
+export * from "./components/icons/Face";
+export * from "./components/icons/Star";
 export * from "./theme";
